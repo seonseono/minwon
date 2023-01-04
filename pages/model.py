bfb5e84ecb801646dbc5f36180a96e25f661061f
dkssudgb <leehb9360@likelion.org>
2023-01-04 17:16:47 +0900

    Update model.py

--- pages/model.py
+++ pages/model.py
@@ -1,8 +1,27 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
+from  PIL import Image
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import io
 
 st.set_page_config(
     page_title="모델 예측 결과",
     layout="wide",
 )
+
+with st.sidebar:
+    choose = option_menu("App Gallery", ["About", "Photo Editing", "Project Planning"],
+                         icons=['house', 'camera fill', 'kanban'],
+                         menu_icon="app-indicator", default_index=0,
+                         styles={
+        "container": {"padding": "5!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "#02ab21"},
+    }
+    )
 
 st.markdown("# 모델 예측 결과")
